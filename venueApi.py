e157d6c087fb0a73943e2e491a2732fab84e158a
Dalmas Chituyi <122692086+dallo7@users.noreply.github.com>
2025-07-24 12:34:32 +0300

    Update venueApi.py

--- venueApi.py
+++ venueApi.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify
-import venueAnalytics
+from flask_cors import CORS 
+import venueAnalytics 
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/venue_report/<string:venue_name>', methods=['GET'])
 def get_venue_report(venue_name):
@@ -11,21 +12,20 @@ def get_venue_report(venue_name):
     Expects the venue name as a path parameter.
     """
     print(f"\n=====================================================")
-    print(f"   API Call Received for Venue: {venue_name}")
+    print(f"    API Call Received for Venue: {venue_name}")
     print(f"=====================================================")
 
-    # Use the first function to get the venue ID
     venue_id = venueAnalytics.get_venue_id_by_name(venue_name)
 
     if venue_id:
-        # Use the second function to generate the analytics report
+     
         analytics_data = venueAnalytics.generate_venue_analytics_report(venue_id)
 
         if "error" in analytics_data:
-            print(f"   ❌ Report generation failed for venue '{venue_name}'.")
+            print(f"    ❌ Report generation failed for venue '{venue_name}'.")
             return jsonify({"status": "error", "message": analytics_data["error"]}), 500
         else:
-            print(f"   ✅ Report generation complete for venue '{venue_name}'.")
+            print(f"    ✅ Report generation complete for venue '{venue_name}'.")
             print("=====================================================")
             return jsonify({"status": "success", "data": analytics_data}), 200
     else:
@@ -34,4 +34,5 @@ def get_venue_report(venue_name):
 
 
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=5060, debug=False, use_reloader=False)
